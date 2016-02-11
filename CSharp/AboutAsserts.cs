adbe6df928f2aea26036a79c0c8994487c7ee135
Walker Dorris <walker.dorris@gmail.com>
2016-02-10 19:16:15 -0800

    nils and assert problems

--- CSharp/AboutAsserts.cs
+++ CSharp/AboutAsserts.cs
@@ -15,7 +15,7 @@ namespace DotNetKoans.CSharp
         [Koan(2)]
         public void AssertTruthWithMessage() 
         {
-            Assert.True(false, "This should be true -- Please fix this");
+            Assert.True(true, "This should be true -- Please fix this");
         }
 
         //To understand reality, we must compare our expectations against reality
@@ -24,14 +24,14 @@ namespace DotNetKoans.CSharp
         {
             var expectedValue = 3;
             var actualValue = 1 + 1;
-            Assert.True(expectedValue == actualValue);
+            Assert.True(expectedValue != actualValue);
         }
 
         //Some wasy of asserting equality are better than others
         [Koan(4)]
         public void ABetterWayOfAssertingEquality() 
         {
-            var expectedValue = 3;
+            var expectedValue = 2;
             var actualValue = 1 + 1;
             Assert.Equal(expectedValue, actualValue);
         }
@@ -40,7 +40,7 @@ namespace DotNetKoans.CSharp
         [Koan(5)]
         public void FillInValues() 
         {
-            Assert.Equal(FILL_ME_IN, 1 + 1);
+            Assert.Equal(2, 1 + 1);
         }
     }
 }

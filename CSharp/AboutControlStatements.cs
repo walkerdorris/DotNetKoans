029e745d4a150568333ae04ffa85be0ecaa33eec
Walker Dorris <walker.dorris@gmail.com>
2016-02-25 12:40:47 -0800

    controlstatements through 8

--- CSharp/AboutControlStatements.cs
+++ CSharp/AboutControlStatements.cs
@@ -76,8 +76,8 @@ namespace DotNetKoans.CSharp
         [Koan(6)]
         public void TernaryOperators()
         {
-            Assert.Equal(FILL_ME_IN, (true ? 1 : 0));
-            Assert.Equal(FILL_ME_IN, (false ? 1 : 0));
+            Assert.Equal(1, (true ? 1 : 0));
+            Assert.Equal(0, (false ? 1 : 0));
         }
 
         //This is out of place for control statements, but necessary for Koan 8
@@ -88,8 +88,8 @@ namespace DotNetKoans.CSharp
             //i = null; //You can't do this
 
             int? nullableInt = null; //but you can do this
-			Assert.NotNull(FILL_ME_IN);
-			Assert.Null(FILL_ME_IN);
+			Assert.NotNull(i);
+			Assert.Null(nullableInt);
         }
 
         [Koan(8)]
@@ -99,7 +99,7 @@ namespace DotNetKoans.CSharp
 
             int x = nullableInt ?? 42;
 
-            Assert.Equal(FILL_ME_IN, x);
+            Assert.Equal(42, x);
         }
 
         [Koan(9)]
